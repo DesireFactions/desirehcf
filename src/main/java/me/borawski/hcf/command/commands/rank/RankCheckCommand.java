aae946bfce516714c3b8adf3ee17f5a63e81dd47
Ryan Radomski <radomski@iastate.edu>
2017-07-15 13:09:07 -0500

    Rank commands now use an API

--- src/main/java/me/borawski/hcf/command/commands/rank/RankCheckCommand.java
+++ src/main/java/me/borawski/hcf/command/commands/rank/RankCheckCommand.java
@@ -1,14 +1,10 @@
 package me.borawski.hcf.command.commands.rank;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
-import me.borawski.hcf.Core;
+import me.borawski.hcf.api.RankAPI;
 import me.borawski.hcf.command.CustomCommand;
 import me.borawski.hcf.session.Rank;
-import me.borawski.hcf.session.Session;
-import me.borawski.hcf.session.SessionHandler;
 
 public class RankCheckCommand extends CustomCommand {
 
@@ -18,19 +14,7 @@ public class RankCheckCommand extends CustomCommand {
 
     @Override
     public void run(CommandSender sender, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(Core.getLangHandler().getString("only-players"));
-            return;
-        }
-
-        Session s = null;
-        try {
-            s = SessionHandler.getSession(((Player) sender).getUniqueId());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        sender.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "DesireHCF " + ChatColor.RESET + "" + ChatColor.GRAY + "Your rank is currently " + ChatColor.YELLOW + s.getRank().getDisplayName());
+        RankAPI.checkRank(sender, label);
     }
 
 }
