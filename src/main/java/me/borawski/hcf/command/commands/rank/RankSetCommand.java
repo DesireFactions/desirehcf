4808b1be8ce87a47cb4ab88b633f2a52217f2262
Ryan Radomski <radomski@iastate.edu>
2017-07-15 13:09:07 -0500

    Rank commands now use an API

--- src/main/java/me/borawski/hcf/command/commands/rank/RankSetCommand.java
+++ src/main/java/me/borawski/hcf/command/commands/rank/RankSetCommand.java
@@ -1,14 +1,10 @@
 package me.borawski.hcf.command.commands.rank;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
+import me.borawski.hcf.api.RankAPI;
 import me.borawski.hcf.command.CustomCommand;
 import me.borawski.hcf.session.Rank;
-import me.borawski.hcf.session.Session;
-import me.borawski.hcf.session.SessionHandler;
-import me.borawski.hcf.util.PlayerUtils;
 
 public class RankSetCommand extends CustomCommand {
 
@@ -21,25 +17,10 @@ public class RankSetCommand extends CustomCommand {
         if (args.length == 2) {
             String name = args[0];
             String rank = args[1];
-            Session s = SessionHandler.getSession(PlayerUtils.getUUIDFromName(name));
-            if (s == null) {
-                System.out.println("[Core] [ERROR] : Could not retrieve " + name);
-                return;
-            }
-            if (Rank.valueOf(rank) == null) {
-                System.out.println("Invalid rank");
-                return;
-            }
-            s.setRank(Rank.valueOf(rank));
-            SessionHandler.getInstance().save(s);
-            sender.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "DesireHCF " + ChatColor.RESET + "" + ChatColor.GRAY + "You have set " + ChatColor.YELLOW + name + "" + ChatColor.GRAY + "'s rank to " + ChatColor.YELLOW + rank);
 
-            if (Bukkit.getPlayer(s.getUniqueId()) != null) {
-                PlayerUtils.setPrefix(s.getRank().getPrefix(), Bukkit.getPlayer(s.getUniqueId()));
-                s.sendMessage(ChatColor.GREEN + "You are now a " + s.getRank().name().toUpperCase());
-            }
+            RankAPI.setRank(sender, label, name, rank);
+        } else {
+            sender.sendMessage(getDescription());
         }
-        // TODO add usage message
     }
-
 }
