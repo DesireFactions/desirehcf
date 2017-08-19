0e1cf875301497efe0e8ad2f1cb824abcf3a8004
Ryan Radomski <radomski@iastate.edu>
2017-08-19 17:03:49 -0500

    fixed constructor in manualcommand

--- src/main/java/me/borawski/hcf/command/commands/base/ManualCommand.java
+++ src/main/java/me/borawski/hcf/command/commands/base/ManualCommand.java
@@ -1,30 +1,17 @@
 package me.borawski.hcf.command.commands.base;
 
-import org.bukkit.command.CommandSender;
-
 import me.borawski.hcf.command.ValidBaseCommand;
+import me.borawski.hcf.command.commands.sub.ManualYoutubeCommand;
 import me.borawski.hcf.session.Rank;
 
 /**
  * Created by Ethan on 5/17/2017.
  */
 public class ManualCommand extends ValidBaseCommand {
-    // TODO make this have sub commands such as yt
 
     public ManualCommand() {
         super("Manual", "Opens a manual", Rank.GUEST);
+        addSubCommand(new ManualYoutubeCommand());
     }
 
-    @Override
-    public void run(CommandSender sender, String label, String[] args) {
-        if (args.length != 1) {
-            LANG.sendUsageMessage(sender, label, "manual");
-        }
-
-        if (args[0].equalsIgnoreCase("yt")) {
-            System.out.println("Attempting to open manual");
-            // TODO what the fuck?
-            // ManualUtil.openManual(Rank.YOUTUBER, (Player) sender);
-        }
-    }
 }
