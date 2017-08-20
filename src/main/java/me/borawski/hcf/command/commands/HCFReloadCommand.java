e8b5cfc5c2aa5471aa23c94f567afdc390a75d0a
Michael Ziluck <ziluckm@iastate.edu>
2017-08-20 13:07:47 -0500

    Rename and complete the lang reload command

--- src/main/java/me/borawski/hcf/command/commands/HCFReloadCommand.java
+++ src/main/java/me/borawski/hcf/command/commands/HCFReloadCommand.java
@@ -2,18 +2,20 @@ package me.borawski.hcf.command.commands;
 
 import org.bukkit.command.CommandSender;
 
+import me.borawski.hcf.api.LangHandler;
 import me.borawski.hcf.command.ValidCommand;
 import me.borawski.hcf.session.Rank;
 
-public class ReloadCommand extends ValidCommand {
+public class HCFReloadCommand extends ValidCommand {
 
-    public ReloadCommand() {
+    public HCFReloadCommand() {
         super("reload", "Reload the lang file.", Rank.ADMIN, new String[] {});
     }
 
     @Override
     public void validRun(CommandSender sender, String label, Object... args) {
-                
+        LangHandler.reloadAll();
+        LANG.sendString(sender, "hcfreload");
     }
 
 }
