005fbd2d768b0590b60e649e2cf8179b54b4e357
Michael Ziluck <ziluckmichael@gmail.com>
2017-08-22 21:25:55 -0500

    TicketHandler loads tickets and sends notifications.

--- src/main/java/me/borawski/hcf/tickets/TicketHandler.java
+++ src/main/java/me/borawski/hcf/tickets/TicketHandler.java
@@ -1,15 +1,34 @@
 package me.borawski.hcf.tickets;
 
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.mongodb.morphia.dao.BasicDAO;
 
 import me.borawski.hcf.Core;
+import me.borawski.hcf.session.Rank;
+import me.borawski.hcf.session.Session;
+import me.borawski.hcf.session.SessionHandler;
 
-public class TicketHandler extends BasicDAO<Ticket, Integer> {
+public class TicketHandler extends BasicDAO<Ticket, Integer> implements Runnable {
 
     public static TicketHandler instance;
 
+    private List<Ticket> openTickets;
+
     public TicketHandler() {
         super(Ticket.class, Core.getInstance().getMongoWrapper().getDatastore());
+        openTickets = find().asList();
+    }
+
+    @Override
+    public void run() {
+        Bukkit.getScheduler().runTaskLater(Core.getInstance(), this, 3600);
+        for (Session s : SessionHandler.getSessions()) {
+            if (s.getRank().getId() >= Rank.MODERATOR.getId()) {
+                Core.getLangHandler().sendRenderMessage(s, "tickets.open", "{number}", String.valueOf(openTickets.size()));
+            }
+        }
     }
 
     public int getNext() {
