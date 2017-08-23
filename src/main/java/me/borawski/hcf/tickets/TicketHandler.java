71cce57901ce0242646eb5f37252a5dbeb4e9f75
Michael Ziluck <ziluckmichael@gmail.com>
2017-08-22 19:05:59 -0500

    Small tweak to punishments and begin work on tickets.

--- src/main/java/me/borawski/hcf/tickets/TicketHandler.java
+++ src/main/java/me/borawski/hcf/tickets/TicketHandler.java
@@ -1,5 +1,32 @@
 package me.borawski.hcf.tickets;
 
-public class TicketHandler {
+import org.mongodb.morphia.dao.BasicDAO;
+
+import me.borawski.hcf.Core;
+
+public class TicketHandler extends BasicDAO<Ticket, Integer> {
+
+    public static TicketHandler instance;
+
+    public TicketHandler() {
+        super(Ticket.class, Core.getInstance().getMongoWrapper().getDatastore());
+    }
+
+    public int getNext() {
+        Ticket t = createQuery().order("-id").get();
+        if (t != null) {
+            return t.getId() + 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void initialize() {
+        instance = new TicketHandler();
+    }
+
+    public static TicketHandler getInstance() {
+        return instance;
+    }
 
 }
