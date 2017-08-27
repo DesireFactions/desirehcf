fbd59195d001e09e53f0d41abb9fcadf722d292f
Michael Ziluck <ziluckmichael@gmail.com>
2017-08-26 20:37:33 -0500

    Chat previously got sent to the console once per player online.

--- src/main/java/me/borawski/hcf/listener/ChatListener.java
+++ src/main/java/me/borawski/hcf/listener/ChatListener.java
@@ -25,32 +25,30 @@ public class ChatListener implements Listener {
 
     @EventHandler
     public void chat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
         event.setCancelled(true);
-        Session s = SessionHandler.getSession(event.getPlayer());
+        Session s = SessionHandler.getSession(player);
         if (s.isMuted() != null) {
             s.sendMessage(ChatColor.DARK_GRAY + "-----------------------------------------------------");
             s.sendMessage("");
-            ChatUtils.sendCenteredMessage(event.getPlayer(), Core.getInstance().getPrefix().replace(" ", ""));
+            ChatUtils.sendCenteredMessage(player, Core.getInstance().getPrefix().replace(" ", ""));
             s.sendMessage("");
-            ChatUtils.sendCenteredMessage(event.getPlayer(), ChatColor.GRAY + "You are muted and " + ChatColor.RED + "CANNOT " + ChatColor.GRAY + "speak!");
-            ChatUtils.sendCenteredMessage(event.getPlayer(), ChatColor.GRAY + "Visit our rules @ " + ChatColor.YELLOW + "https://desirehcf.net/rules");
+            ChatUtils.sendCenteredMessage(player, ChatColor.GRAY + "You are muted and " + ChatColor.RED + "CANNOT " + ChatColor.GRAY + "speak!");
+            ChatUtils.sendCenteredMessage(player, ChatColor.GRAY + "Visit our rules @ " + ChatColor.YELLOW + "https://desirehcf.net/rules");
             s.sendMessage("");
             s.sendMessage(ChatColor.DARK_GRAY + "-----------------------------------------------------");
             return;
         }
 
         String msg = event.getMessage();
-        Player player = event.getPlayer();
 
+        Faction f = FactionsUtils.getFaction(player);
+
+        String parsedMessage = s.getRank().getId() >= Rank.ADMIN.getId() ? ChatColor.translateAlternateColorCodes('&', msg) : msg;
+        System.out.println(player.getName() + ": " + parsedMessage);
         Bukkit.getOnlinePlayers().stream().forEach(new Consumer<Player>() {
             @Override
             public void accept(Player players) {
-                Session s = SessionHandler.getSession(player);
-                Faction f = FactionsUtils.getFaction(player);
-
-                String parsedMessage = s.getRank().getId() >= Rank.ADMIN.getId() ? ChatColor.translateAlternateColorCodes('&', msg) : msg;
-
-                System.out.println(player.getName() + ": " + parsedMessage);
 
                 if (f.getId().equals("0")) {
                     new FancyMessage(s.getRank().getPrefix())
