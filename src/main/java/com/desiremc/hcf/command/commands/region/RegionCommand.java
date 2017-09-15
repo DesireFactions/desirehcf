6f93e0a9d966aef95898bcc51312936666427ca2
Michael Ziluck <ziluckm@iastate.edu>
2017-09-15 18:16:21 -0500

    Remove unfinished commands.

--- src/main/java/com/desiremc/hcf/command/commands/region/RegionCommand.java
+++ src/main/java/com/desiremc/hcf/command/commands/region/RegionCommand.java
@@ -8,9 +8,6 @@ public class RegionCommand extends ValidBaseCommand {
     public RegionCommand() {
         super("hregion", "Manage the protection regions.", Rank.ADMIN, "hregions", "hrg");
         addSubCommand(new RegionCreateCommand());
-        addSubCommand(new RegionDeleteCommand());
-        addSubCommand(new RegionListCommand());
-        addSubCommand(new RegionModifyCommand());
     }
 
 }
