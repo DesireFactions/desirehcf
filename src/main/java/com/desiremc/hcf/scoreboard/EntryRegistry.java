6a207a4f49acfcb969260b3e54a101894d788a08
Michael Ziluck <ziluckm@iastate.edu>
2017-09-17 12:56:09 -0500

    Add new scoreboard system.

---
+++ src/main/java/com/desiremc/hcf/scoreboard/EntryRegistry.java
@@ -0,0 +1,148 @@
+package com.desiremc.hcf.scoreboard;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
+import com.desiremc.hcf.scoreboard.common.EntryBuilder;
+import com.desiremc.hcf.scoreboard.type.Entry;
+import com.desiremc.hcf.scoreboard.type.Scoreboard;
+import com.desiremc.hcf.scoreboard.type.ScoreboardHandler;
+import com.desiremc.hcf.scoreboard.type.SimpleScoreboard;
+
+public class EntryRegistry implements ScoreboardHandler
+{
+
+    private static EntryRegistry instance;
+
+    private HashMap<Player, PlayerEntry> entries = new HashMap<>();
+
+    @Override
+    public String getTitle(Player player)
+    {
+        return "§3Desire §cHCF";
+    }
+
+    @Override
+    public List<Entry> getEntries(Player player)
+    {
+        Collection<String> entry = entries.get(player).getEntries();
+        if (entry == null)
+        {
+            entry = new LinkedList<>();
+        }
+
+        return EntryBuilder.build(entry);
+    }
+
+    @Override
+    public boolean hasEntries(Player player)
+    {
+        PlayerEntry entry = entries.get(player);
+
+        if (entry == null) { return false; }
+        if (entry.getEntries() == null) { return false; }
+
+        return entry.getEntries().size() != 0;
+    }
+
+    public PlayerEntry getEntry(Player player)
+    {
+        return entries.get(player);
+    }
+
+    /**
+     * Set the value on a player's scoreboard.
+     * 
+     * @param player
+     *            the player to target
+     * @param key
+     *            the key to store as reference
+     * @param value
+     *            the string displayed on the scoreboard.
+     */
+    public void setValue(Player player, String key, String value)
+    {
+        PlayerEntry entry = getEntry(player);
+        if (entry == null)
+        {
+            entry = new PlayerEntry();
+            entries.put(player, entry);
+            Scoreboard board = new SimpleScoreboard(player).setHandler(instance).setUpdateInterval(2l);
+            board.activate();
+        }
+        entry.setEntry(key, value);
+    }
+
+    /**
+     * Clear a value on a player's scoreboard.
+     * 
+     * @param player
+     *            the player to target
+     * @param key
+     *            the key used as a reference
+     */
+    public void removeValue(Player player, String key)
+    {
+        PlayerEntry entry = getEntry(player);
+        if (entry != null)
+        {
+            entry.clearEntry(key);
+
+            if (!entry.hasEntries())
+            {
+                ScoreboardRegistry.getInstance().clearScoreboard(player);
+            }
+        }
+    }
+
+    public static void initialize()
+    {
+        instance = new EntryRegistry();
+    }
+
+    public static EntryRegistry getInstance()
+    {
+        return instance;
+    }
+
+    public static class PlayerEntry
+    {
+
+        private HashMap<String, String> entries;
+
+        public PlayerEntry()
+        {
+            this.entries = new HashMap<>();
+        }
+
+        public String getEntry(String key)
+        {
+            return entries.get(key);
+        }
+
+        public void setEntry(String key, String value)
+        {
+            entries.put(key, value);
+        }
+
+        public void clearEntry(String key)
+        {
+            entries.remove(key);
+        }
+
+        public Collection<String> getEntries()
+        {
+            return entries.values();
+        }
+
+        public boolean hasEntries()
+        {
+            return entries.size() > 0;
+        }
+    }
+
+}
---
+++ src/main/java/com/desiremc/hcf/scoreboard/type/Entry.java
@@ -0,0 +1,31 @@
+package com.desiremc.hcf.scoreboard.type;
+
+import com.desiremc.hcf.scoreboard.common.Strings;
+
+public class Entry {
+
+    private String name;
+    private int position;
+
+    public Entry(String name, int position) {
+        this.name = Strings.format(name);
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+}
