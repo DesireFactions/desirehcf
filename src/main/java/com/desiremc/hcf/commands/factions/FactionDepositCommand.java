ecacb056694277017b99a122752988f60d20368f
ctooley21 <christian.tooley@uky.edu>
2017-12-28 10:10:06 -0600

    DESIRE-257

--- src/main/java/com/desiremc/hcf/commands/factions/FactionDepositCommand.java
+++ src/main/java/com/desiremc/hcf/commands/factions/FactionDepositCommand.java
@@ -1,30 +1,30 @@
 package com.desiremc.hcf.commands.factions;
 
+import java.util.List;
+
 import com.desiremc.core.api.newcommands.CommandArgument;
 import com.desiremc.core.api.newcommands.CommandArgumentBuilder;
-import com.desiremc.core.parsers.IntegerParser;
+import com.desiremc.core.parsers.StringParser;
 import com.desiremc.core.utils.StringUtils;
 import com.desiremc.hcf.DesireHCF;
 import com.desiremc.hcf.api.commands.FactionValidCommand;
 import com.desiremc.hcf.session.FSession;
 import com.desiremc.hcf.session.faction.Faction;
-import com.desiremc.hcf.validators.SenderHasAmountMoney;
 import com.desiremc.hcf.validators.SenderHasFactionValidator;
 
-import java.util.List;
+import net.minecraft.util.com.google.common.primitives.Doubles;
 
 public class FactionDepositCommand extends FactionValidCommand
 {
     public FactionDepositCommand()
     {
-        super("deposit", "Deposit money to your faction.", true);
+        super("deposit", "Deposit money to your faction.", true, new String[] {"d"});
 
         addSenderValidator(new SenderHasFactionValidator());
 
-        addArgument(CommandArgumentBuilder.createBuilder(Integer.class)
+        addArgument(CommandArgumentBuilder.createBuilder(String.class)
                 .setName("amount")
-                .setParser(new IntegerParser())
-                .addValidator(new SenderHasAmountMoney())
+                .setParser(new StringParser())
                 .build());
     }
 
@@ -32,7 +32,8 @@ public class FactionDepositCommand extends FactionValidCommand
     public void validFactionRun(FSession sender, String[] label, List<CommandArgument<?>> arguments)
     {
         Faction faction = sender.getFaction();
-        double amount = ((Number) arguments.get(0).getValue()).doubleValue();
+        String arg = (String) arguments.get(0).getValue();
+        double amount = Doubles.tryParse(arg);
 
         faction.depositBalance(amount);
         sender.withdrawBalance(amount);
--- src/main/java/com/desiremc/hcf/validators/SenderHasAmountMoney.java
+++ src/main/java/com/desiremc/hcf/validators/SenderHasAmountMoney.java
@@ -4,12 +4,33 @@ import com.desiremc.hcf.DesireHCF;
 import com.desiremc.hcf.api.commands.FactionValidator;
 import com.desiremc.hcf.session.FSession;
 
-public class SenderHasAmountMoney extends FactionValidator<Integer>
+import net.minecraft.util.com.google.common.primitives.Doubles;
+
+public class SenderHasAmountMoney extends FactionValidator<String>
 {
     @Override
-    public boolean factionsValidateArgument(FSession sender, String[] label, Integer arg)
+    public boolean factionsValidateArgument(FSession sender, String[] label, String arg)
     {
-        if (sender.getBalance() < arg)
+        double amount;
+        if (arg.equalsIgnoreCase("all"))
+        {
+            amount = sender.getBalance();
+        }
+        else
+        {
+            if (Doubles.tryParse(arg) == null)
+            {
+                DesireHCF.getLangHandler().sendRenderMessage(sender.getSession(), "factions.invalid_deposit", true, false);
+                return false;
+            }
+            else
+            {
+                amount = Doubles.tryParse(arg);
+            }
+        }
+
+
+        if (sender.getBalance() < amount)
         {
             DesireHCF.getLangHandler().sendRenderMessage(sender.getSession(), "factions.no_money", true, false);
             return false;
