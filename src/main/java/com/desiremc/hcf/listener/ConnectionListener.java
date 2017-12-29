b4349c1011e3786d76ce892405ee73ffe1ff9873
ctooley21 <christian.tooley@uky.edu>
2017-12-28 21:46:54 -0600

    Testing

--- src/main/java/com/desiremc/hcf/listener/ConnectionListener.java
+++ src/main/java/com/desiremc/hcf/listener/ConnectionListener.java
@@ -97,7 +97,6 @@ public class ConnectionListener implements Listener
             fSession.getClaimSession().delete();
             fSession.getClaimSession().run();
         }
-        fSession.save();
         FSessionHandler.endSession(fSession);
     }
 
