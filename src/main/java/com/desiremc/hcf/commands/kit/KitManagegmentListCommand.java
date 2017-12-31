db4878014c4ac4aedfa0ffc81d2b7c483cffa743
ctooley21 <christian.tooley@uky.edu>
2017-12-31 16:26:06 -0600

    Finish kit list command

--- src/main/java/com/desiremc/hcf/commands/kit/KitManagegmentListCommand.java
+++ src/main/java/com/desiremc/hcf/commands/kit/KitManagegmentListCommand.java
@@ -6,6 +6,7 @@ import com.desiremc.core.api.newcommands.CommandArgument;
 import com.desiremc.core.api.newcommands.ValidCommand;
 import com.desiremc.core.session.Rank;
 import com.desiremc.core.session.Session;
+import com.desiremc.hcf.DesireHCF;
 import com.desiremc.hcf.session.HKit;
 import com.desiremc.hcf.session.HKitHandler;
 
@@ -19,9 +20,16 @@ public class KitManagegmentListCommand extends ValidCommand
     @Override
     public void validRun(Session sender, String label[], List<CommandArgument<?>> args)
     {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("§b");
         for (HKit kit : HKitHandler.getKits())
         {
-            sender.sendMessage(kit.getName());
+            sb.append(kit.getName());
+            sb.append("§7, §b");
         }
+        sb.setLength(sb.length() - 6);
+
+        DesireHCF.getLangHandler().sendRenderMessage(sender, "kits.list", true, false, "{kits}", sb.toString());
     }
 }
