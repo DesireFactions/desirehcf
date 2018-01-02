1cec72114b779c41ada1a6fdd0b7f078aed6498d
ctooley21 <christian.tooley@uky.edu>
2018-01-02 13:18:22 -0600

    meh

--- src/main/java/com/desiremc/hcf/listener/factions/GeneralListener.java
+++ src/main/java/com/desiremc/hcf/listener/factions/GeneralListener.java
@@ -14,7 +14,6 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
@@ -65,25 +64,6 @@ public class GeneralListener implements Listener
     public void onTntExplode(EntityExplodeEvent event)
     {
         event.blockList().clear();
-    }
-
-    @EventHandler
-    public void onInteract(PlayerInteractEvent event)
-    {
-        if (event.getItem() != null && event.getItem().getType().equals(Material.FISHING_ROD))
-        {
-            return;
-        }
-
-        Player player = event.getPlayer();
-        Faction faction = FactionsUtils.getFaction(player.getLocation());
-        FSession session = FSessionHandler.getOnlineFSession(player.getUniqueId());
-
-        if ((faction.isNormal() && faction.getMembers().contains(session)) || faction.isWilderness())
-        {
-            return;
-        }
-        event.setCancelled(true);
     }
 
     @EventHandler
