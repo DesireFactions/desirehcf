c240cbb42d97a109846fa0d7549700dc2a378bdb
ctooley21 <christian.tooley@uky.edu>
2018-01-03 13:46:49 -0600

    air not cancel?

--- src/main/java/com/desiremc/hcf/listener/factions/GeneralListener.java
+++ src/main/java/com/desiremc/hcf/listener/factions/GeneralListener.java
@@ -3,7 +3,6 @@ package com.desiremc.hcf.listener.factions;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
@@ -100,15 +99,14 @@ public class GeneralListener implements Listener
     @EventHandler
     public void onBlockFromToEvent(BlockFromToEvent event)
     {
-        Block block = event.getToBlock();
-        if (block.getType().equals(Material.STATIONARY_WATER) || block.getType().equals(Material.STATIONARY_LAVA)
-                || block.getType().equals(Material.WATER) || block.getType().equals(Material.LAVA))
+        int id = event.getBlock().getTypeId();
+        if (id == 8 || id == 9 || id == 10 || id == 11)
         {
             Location loc = event.getToBlock().getLocation();
             Faction faction = FactionHandler.getFaction(loc);
             if (faction.getType() != FactionType.PLAYER)
             {
-                block.setType(Material.AIR);
+                event.setCancelled(true);
             }
         }
     }
