9a9b7f2231dc59055dd7bbea3bf3a2aaf6bd69ed
ctooley21 <christian.tooley@uky.edu>
2018-01-03 12:12:32 -0600

    Remove debug code

--- src/main/java/com/desiremc/hcf/session/FSession.java
+++ src/main/java/com/desiremc/hcf/session/FSession.java
@@ -325,7 +325,6 @@ public class FSession implements Messageable
 
     public DeathBan getActiveDeathBan()
     {
-        Bukkit.broadcastMessage(DesireHCF.getInstance().getServer().getName());
         return DeathBanHandler.getDeathBan(getSession(), DesireCore.getCurrentServer());
     }
 
