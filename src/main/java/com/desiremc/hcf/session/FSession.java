0484ea92e6896b8c4e80015549429bcf68fd7ce3
ctooley21 <christian.tooley@uky.edu>
2018-01-03 12:11:20 -0600

    wrong name..

--- src/main/java/com/desiremc/hcf/session/FSession.java
+++ src/main/java/com/desiremc/hcf/session/FSession.java
@@ -326,7 +326,7 @@ public class FSession implements Messageable
     public DeathBan getActiveDeathBan()
     {
         Bukkit.broadcastMessage(DesireHCF.getInstance().getServer().getName());
-        return DeathBanHandler.getDeathBan(getSession(), DesireHCF.getInstance().getServer().getName());
+        return DeathBanHandler.getDeathBan(getSession(), DesireCore.getCurrentServer());
     }
 
     public void revive(String reson, boolean staffRevived, UUID reviver)
