538323875e24cf6f95ee192859a2d6f5fdbf85c4
ctooley21 <christian.tooley@uky.edu>
2018-01-04 16:16:11 -0600

    let players build past 350

--- src/main/java/com/desiremc/hcf/listener/factions/PlayerListener.java
+++ src/main/java/com/desiremc/hcf/listener/factions/PlayerListener.java
@@ -566,7 +566,7 @@ public class PlayerListener implements Listener
         {
             double x = Math.abs(location.getX());
             double z = Math.abs(location.getZ());
-            if (x < 350 || z < 350)
+            if ((x < 350 && z < 350) || otherFaction.getName().contains("Road"))
             {
                 DesireHCF.getLangHandler().sendRenderMessage(fSession.getSession(), "factions.protection.build", true, false);
                 return false;
@@ -616,7 +616,7 @@ public class PlayerListener implements Listener
         {
             double x = Math.abs(location.getX());
             double z = Math.abs(location.getZ());
-            if (x < 350 || z < 350)
+            if ((x < 350 && z < 350) || otherFaction.getName().contains("Road"))
             {
                 DesireHCF.getLangHandler().sendRenderMessage(fSession.getSession(), "factions.protection.build", true, false);
                 return false;
