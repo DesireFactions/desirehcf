e01689ada40dd8fc206b8bfbe1818786e7cf7eb2
ctooley21 <christian.tooley@uky.edu>
2018-01-03 20:49:09 -0600

    Not too big buddy

--- src/main/java/com/desiremc/hcf/commands/factions/FactionNameCommand.java
+++ src/main/java/com/desiremc/hcf/commands/factions/FactionNameCommand.java
@@ -7,6 +7,7 @@ import com.desiremc.core.api.newcommands.CommandArgumentBuilder;
 import com.desiremc.core.parsers.StringParser;
 import com.desiremc.core.session.Achievement;
 import com.desiremc.core.session.Rank;
+import com.desiremc.core.validators.StringLengthValidator;
 import com.desiremc.hcf.DesireHCF;
 import com.desiremc.hcf.api.commands.FactionValidCommand;
 import com.desiremc.hcf.session.FSession;
@@ -29,6 +30,7 @@ public class FactionNameCommand extends FactionValidCommand
                 .setName("name")
                 .setParser(new StringParser())
                 .addValidator(new FactionNameNotTaken())
+                .addValidator(new StringLengthValidator(DesireHCF.getConfigHandler().getInteger("factions.names.length.min"), DesireHCF.getConfigHandler().getInteger("factions.names.length.max")))
                 .build());
     }
 
