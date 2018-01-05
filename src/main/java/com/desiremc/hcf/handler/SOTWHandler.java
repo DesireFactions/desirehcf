c2088939f1f0c0514744ff7555b6009f68abe03a
ctooley21 <christian.tooley@uky.edu>
2018-01-05 12:20:50 -0600

    Prevent Immunity timer from going down during SOTW

--- src/main/java/com/desiremc/hcf/handler/SOTWHandler.java
+++ src/main/java/com/desiremc/hcf/handler/SOTWHandler.java
@@ -1,6 +1,8 @@
 package com.desiremc.hcf.handler;
 
 import com.desiremc.hcf.DesireHCF;
+import com.desiremc.hcf.session.FSession;
+import com.desiremc.hcf.session.FSessionHandler;
 import com.desiremc.hcf.tasks.SOTWTask;
 
 public class SOTWHandler
@@ -28,6 +30,12 @@ public class SOTWHandler
     {
         sotw = true;
         new SOTWTask().runTaskTimer(DesireHCF.getInstance(), 0L, 20L);
+
+        for (FSession fSession : FSessionHandler.getOnlineFSessions()) {
+            if (fSession.getSafeTimeLeft() > 0) {
+                fSession.getSafeTimer().pause();
+            }
+        }
     }
 
     public static void initialize()
