3bd05de7e6c6987eac183a1d07f21a4749c8238c
ctooley21 <christian.tooley@uky.edu>
2018-01-05 13:03:33 -0600

    wrong plugin

--- src/main/java/com/desiremc/hcf/commands/HelpOpCommand.java
+++ src/main/java/com/desiremc/hcf/commands/HelpOpCommand.java
@@ -1,14 +1,14 @@
 package com.desiremc.hcf.commands;
 
-import java.util.List;
-
-import com.desiremc.core.DesireCore;
 import com.desiremc.core.api.newcommands.CommandArgument;
 import com.desiremc.core.api.newcommands.CommandArgumentBuilder;
 import com.desiremc.core.api.newcommands.ValidCommand;
 import com.desiremc.core.parsers.StringParser;
 import com.desiremc.core.session.Session;
 import com.desiremc.core.session.SessionHandler;
+import com.desiremc.hcf.DesireHCF;
+
+import java.util.List;
 
 public class HelpOpCommand extends ValidCommand
 {
@@ -27,13 +27,13 @@ public class HelpOpCommand extends ValidCommand
     @Override
     public void validRun(Session sender, String[] label, List<CommandArgument<?>> arguments)
     {
-        String message = DesireCore.getLangHandler().renderMessage("helpop.target", false, false, "{player}", sender.getName(), "{message}", arguments.get(0).getValue());
+        String message = DesireHCF.getLangHandler().renderMessage("helpop.target", false, false, "{player}", sender.getName(), "{message}", arguments.get(0).getValue());
 
         for (Session staff : SessionHandler.getOnlineStaff())
         {
             staff.sendMessage(message);
         }
 
-        DesireCore.getLangHandler().sendRenderMessage(sender, "helpop.self", true, false);
+        DesireHCF.getLangHandler().sendRenderMessage(sender, "helpop.self", true, false);
     }
 }
