b6155563e1c18906a4877437a5b68efde8df3741
ctooley21 <christian.tooley@uky.edu>
2018-01-05 13:00:38 -0600

    Fix FactionStuckCommand validator

--- src/main/java/com/desiremc/hcf/validators/SenderCanFactionStuck.java
+++ src/main/java/com/desiremc/hcf/validators/SenderCanFactionStuck.java
@@ -3,6 +3,8 @@ package com.desiremc.hcf.validators;
 import com.desiremc.hcf.DesireHCF;
 import com.desiremc.hcf.api.commands.FactionSenderValidator;
 import com.desiremc.hcf.session.FSession;
+import com.desiremc.hcf.session.faction.Faction;
+import com.desiremc.hcf.session.faction.FactionHandler;
 import org.bukkit.Location;
 import org.bukkit.World;
 
@@ -10,7 +12,8 @@ public class SenderCanFactionStuck extends FactionSenderValidator {
     @Override
     public boolean factionsValidate(FSession sender) {
         Location loc = sender.getLocation();
-        if (loc.getWorld().getEnvironment() == World.Environment.THE_END) {
+        Faction faction = FactionHandler.getFaction(loc);
+        if (loc.getWorld().getEnvironment() == World.Environment.THE_END || faction == sender.getFaction()) {
             DesireHCF.getLangHandler().sendRenderMessage(sender, "factions.stuck.invalid", true, false);
             return false;
         }
